764be28a468d45c77cf849329007e85ea7d0c3d1
eunseopsong <lexondms1@g.skku.edu>
2025-09-24 13:46:59 +0900

    trying BC (1)

--- source/nrs_lab2/nrs_lab2/tasks/manager_based/nrs_lab2/config/ur10e_spindle/polishing_env_cfg.py
+++ source/nrs_lab2/nrs_lab2/tasks/manager_based/nrs_lab2/config/ur10e_spindle/polishing_env_cfg.py
@@ -1,15 +1,13 @@
 # SPDX-License-Identifier: BSD-3-Clause
 """
-UR10e + Spindle: Joint-Hold 환경 설정
+UR10e + Spindle: BC 실행 환경
 """
 
 from __future__ import annotations
 from isaaclab.utils import configclass
-
 from nrs_lab2.nrs_lab2.tasks.manager_based.nrs_lab2.ur10e_spindle_env_cfg import UR10eSpindleEnvCfg
-from nrs_lab2.nrs_lab2.tasks.manager_based.nrs_lab2.mdp import rewards as local_rewards
+from nrs_lab2.nrs_lab2.tasks.manager_based.nrs_lab2 import rewards as local_rewards
 from isaaclab.managers import EventTermCfg as EventTerm
-
 
 @configclass
 class PolishingPoseHoldEnvCfg(UR10eSpindleEnvCfg):
@@ -20,21 +18,9 @@ class PolishingPoseHoldEnvCfg(UR10eSpindleEnvCfg):
         self.sim.render_interval = self.decimation
         self.episode_length_s = 30.0
 
-        # ✅ dataset_key 수정됨
-        self.events.load_hdf5 = EventTerm(
-            func=local_rewards.load_hdf5_trajectory,
+        # ✅ Reset 시 BC policy 불러오기
+        self.events.load_bc = EventTerm(
+            func=local_rewards.load_bc_policy,
             mode="reset",
-            params={
-                "file_path": "/home/eunseop/nrs_lab2/datasets/joint_recording.h5",
-                "dataset_key": "joint_positions",  # 🔥 여기 맞춤
-            },
+            params={"file_path": "/home/eunseop/nrs_lab2/datasets/bc_policy.pth"},
         )
-
-
-@configclass
-class PolishingPoseHoldEnvCfg_PLAY(PolishingPoseHoldEnvCfg):
-    def __post_init__(self):
-        super().__post_init__()
-        self.scene.num_envs = 50
-        self.scene.env_spacing = 2.5
-        self.observations.policy.enable_corruption = False
